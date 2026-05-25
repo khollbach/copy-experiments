b7850d9398e2f4dd49500b983095c7a59959fa49
Kevan Hollbach <khollbach@gmail.com>
2026-05-25 12:30:40 -0400

    Compare my snappy impl to the reference impl

--- benches/bench.rs
+++ benches/bench.rs
@@ -8,7 +8,8 @@ use std::{
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 
 criterion_main!(benches);
-criterion_group!(benches, buffer_size, misc, compression);
+// criterion_group!(benches, buffer_size, misc, compression);
+criterion_group!(benches, compression);
 
 fn buffer_size(c: &mut Criterion) {
     let mut g = c.benchmark_group("buffer size");
@@ -100,96 +101,56 @@ fn write(output: &mut [u8], value: u8) {
 }
 
 fn compression(c: &mut Criterion) {
-    let mut g = c.benchmark_group("compression");
+    let mut g = c.benchmark_group("text compression");
 
-    // let n = 1024 * 1024 * 1024; // 1 Gi
-    let n = 128 * 1024 * 1024; // 128 Mi
+    let m = 1024 * 1024;
+    for n in [32 * m, 64 * m, 128 * m] {
+        // for n in [m] {
+        // let zeros = vec![0; n];
 
-    let zeros = vec![0; n];
+        // let mut random = vec![0; n];
+        // rand::fill(&mut random);
 
-    let mut random = vec![0; n];
-    rand::fill(&mut random);
-
-    let mut text = Vec::with_capacity(n);
-    while text.len() < n {
-        let alice = include_bytes!("../alice29.txt");
-        let chunk_len = min(alice.len(), n - text.len());
-        text.extend_from_slice(&alice[..chunk_len]);
-    }
-
-    // for (input_name, input) in [("text", text)] {
-    for (input_name, input) in [("zeros", zeros), ("random", random), ("text", text)] {
-        let mut output = vec![0; n * 2];
-
-        // for comparison
-        {
-            // (only a few microseconds; doesn't come into the picture)
-            // let id = BenchmarkId::new("alloc", input_name);
-            // g.bench_function(id, |b| b.iter(|| alloc_and_zero(n)));
-
-            let id = BenchmarkId::new("memcpy", input_name);
-            g.bench_function(id, |b| b.iter(|| copy(&input, &mut output[..n])));
+        let mut text = Vec::with_capacity(n);
+        while text.len() < n {
+            let alice = include_bytes!("../alice29.txt");
+            let chunk_len = min(alice.len(), n - text.len());
+            text.extend_from_slice(&alice[..chunk_len]);
         }
 
-        {
+        // for (input_name, input) in [("zeros", zeros), ("random", random), ("text", text)] {
+        for (_input_name, input) in [("text", text)] {
+            let mut output = vec![0; n * 2];
+
+            let id = BenchmarkId::new("memcpy", n);
+            g.bench_function(id, |b| b.iter(|| copy(&input, &mut output[..n])));
+
             let mut compressed_input = vec![0; n * 2];
             let len = snap::raw::Encoder::new()
                 .compress(&input, &mut compressed_input)
                 .unwrap();
             compressed_input.truncate(len);
 
-            let id = BenchmarkId::new("my_compress", input_name);
+            let id = BenchmarkId::new("compress", n);
             g.bench_function(id, |b| b.iter(|| my_compress(&input, &mut output)));
 
-            let id = BenchmarkId::new("my_decompress", input_name);
+            let id = BenchmarkId::new("compress_reference_impl", n);
+            g.bench_function(id, |b| b.iter(|| snap_compress(&input, &mut output)));
+
+            let id = BenchmarkId::new("decompress", n);
             g.bench_function(id, |b| {
                 b.iter(|| my_decompress(&compressed_input, &mut output))
             });
 
-            let id = BenchmarkId::new("snap_compress", input_name);
-            g.bench_function(id, |b| b.iter(|| snap_compress(&input, &mut output)));
-
-            let id = BenchmarkId::new("snap_decompress", input_name);
+            let id = BenchmarkId::new("decompress_reference_impl", n);
             g.bench_function(id, |b| {
                 b.iter(|| snap_decompress(&compressed_input, &mut output))
-            });
-        }
-
-        // (Too slow!)
-        // {
-        //     let mut compressed_input = vec![0; n * 2];
-        //     let len = deflate_compress(&input, &mut compressed_input);
-        //     compressed_input.truncate(len);
-
-        //     let id = BenchmarkId::new("deflate_compress", input_name);
-        //     g.bench_function(id, |b| b.iter(|| deflate_compress(&input, &mut output)));
-
-        //     let id = BenchmarkId::new("deflate_decompress", input_name);
-        //     g.bench_function(id, |b| {
-        //         b.iter(|| deflate_decompress(&compressed_input, &mut output))
-        //     });
-        // }
-
-        {
-            let compressed_input = zstd::stream::encode_all(input.as_slice(), 0).unwrap();
-
-            let id = BenchmarkId::new("zstd_compress", input_name);
-            g.bench_function(id, |b| b.iter(|| zstd_compress(&input, &mut output)));
-
-            let id = BenchmarkId::new("zstd_decompress", input_name);
-            g.bench_function(id, |b| {
-                b.iter(|| zstd_decompress(&compressed_input, &mut output))
             });
         }
     }
 
     g.finish();
 }
-
-// fn alloc_and_zero(n: usize) {
-//     let buf = vec![0; n];
-//     hint::black_box(buf);
-// }
 
 fn my_compress(input: &[u8], mut output: &mut [u8]) {
     // todo: extra copy -- does it matter?
@@ -202,45 +163,10 @@ fn my_decompress(input: &[u8], mut output: &mut [u8]) {
     output.write_all(&buf).unwrap();
 }
 
-//
-
 fn snap_compress(input: &[u8], output: &mut [u8]) {
     snap::raw::Encoder::new().compress(input, output).unwrap();
 }
 
 fn snap_decompress(input: &[u8], output: &mut [u8]) {
     snap::raw::Decoder::new().decompress(input, output).unwrap();
-}
-
-//
-
-// fn deflate_compress(input: &[u8], output: &mut [u8]) -> usize {
-//     let mut co = flate2::Compress::new(flate2::Compression::fast(), false);
-//     let st = co
-//         .compress(input, output, flate2::FlushCompress::None)
-//         .unwrap();
-//     assert_eq!(st, flate2::Status::Ok);
-//     assert_eq!(co.total_in(), u64::try_from(input.len()).unwrap());
-//     usize::try_from(co.total_out()).unwrap()
-// }
-
-// fn deflate_decompress(input: &[u8], output: &mut [u8]) {
-//     let mut de = flate2::Decompress::new(false);
-//     let st = de
-//         .decompress(input, output, flate2::FlushDecompress::None)
-//         .unwrap();
-//     assert_eq!(st, flate2::Status::Ok);
-//     assert_eq!(de.total_in(), u64::try_from(input.len()).unwrap());
-// }
-
-//
-
-fn zstd_compress(input: &[u8], mut output: &mut [u8]) {
-    let buf = zstd::stream::encode_all(input, 0).unwrap();
-    output.write_all(&buf).unwrap();
-}
-
-fn zstd_decompress(input: &[u8], mut output: &mut [u8]) {
-    let buf = zstd::stream::decode_all(input).unwrap();
-    output.write_all(&buf).unwrap();
 }
